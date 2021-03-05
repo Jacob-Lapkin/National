28bc75532ec85764f81087aef7ce4cfe8a813b39
Post Shalom <jacobglapkin@gmail.com>
2021-03-05 14:14:14 +0000

    weather

--- js/main.js
+++ js/main.js
@@ -9,25 +9,6 @@ today = mm + '/' + dd + '/' + yyyy;
     document.getElementById("current-date").innerHTML = today;
 
 
-// Weather API
-var button = document.querySelector('.btn')
-        var inputValue = document.querySelector('.inputValue')
-        var name = document.querySelector('.name');
-        var desc = document.querySelector('.desc');
-        var temp = document.querySelector('.temp');
-
-        button.addEventListener('click', function(){
-        fetch('https://api.openweathermap.org/data/2.5/weather?q=Yellowstone&units=imperial&appid=570830c0c8ec32c1824ab7c214e2493f')
-        .then(response => response.json())
-        .then(data => {
-            var nameValue = data['name'];
-            var tempValue = data['main']['temp'];
-            var descValue = data['weather'][0]['description'];
-
-            temp.innerHTML = tempValue + ' F';
-            desc.innerHTML = descValue;
-        })
-        } )
 
 //remove temperature button 
 $(function () {
@@ -166,28 +147,28 @@ function initMap() {
 
     //STARTING COLORADO
     // The location of Rocky
-    var uluru = { lat: 40.342, lng: -105.68};
+    var rocky = { lat: 40.342, lng: -105.68};
     // The map, centered at Rocky
     var map = new google.maps.Map(document.getElementById("map1"), {
       zoom: 4,
-      center: uluru,
+      center: rocky,
     });
     // The marker, positioned at Rocky
     var marker = new google.maps.Marker({
-      position: uluru,
+      position: rocky,
       map: map,
     });
 
     // The location of Zion
-    var uluru = { lat: 37.298, lng: -113.02};
+    var zion = { lat: 37.298, lng: -113.02};
     // The map, centered at Zion
     var map = new google.maps.Map(document.getElementById("map2"), {
       zoom: 4,
-      center: uluru,
+      center: zion,
     });
     // The marker, positioned at Zion
     var marker = new google.maps.Marker({
-      position: uluru,
+      position: zion,
       map: map,
     });
   }

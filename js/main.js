cfb45c4feba88522e3ee8c4f08e2381a0b6099a1
Post Shalom <jacobglapkin@gmail.com>
2021-03-06 12:08:48 +0000

    hawaii temp

--- js/main.js
+++ js/main.js
@@ -73,6 +73,22 @@ today = mm + '/' + dd + '/' + yyyy;
     document.getElementById('temp-iconaca').innerHTML = '<img src="http://openweathermap.org/img/w/' + iconValue + '.png"/>';
   })
 
+  //temperature volcano 
+
+  fetch('https://api.openweathermap.org/data/2.5/weather?q=Volcano&appid=570830c0c8ec32c1824ab7c214e2493f&units=imperial')  
+  .then(function(resp) { return resp.json() }) // Convert data to json
+  .then(data =>  {
+    var tempValue = data['main']['temp'];
+    var descValue = data['weather'][0]['description'];
+    var iconValue = data['weather'][0]['icon']
+
+
+    console.log(data)
+    tempvol.innerHTML = tempValue + ' F';
+    descvol.innerHTML = descValue;
+    document.getElementById('temp-iconvol').innerHTML = '<img src="http://openweathermap.org/img/w/' + iconValue + '.png"/>';
+  })
+
 
 
 
